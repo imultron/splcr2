37b48aa043c102f5f0523e8cc56cb4a671536262
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-08-13 22:06:40 +0530

    big change 2 files

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-document.getElementById('menuIcon').addEventListener('click', function() {
+document.getElementById('menuIcon').addEventListener('click', function () {
     var navLinks = document.getElementById('navLinks');
     navLinks.classList.toggle('show');
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('audioElement');
     const playButton = document.getElementById('play');
     const stopButton = document.getElementById('stop');
     const speakerIcon = document.getElementById('SpeakerIcon');
     const para = document.querySelector('.para');
 
-
-    playButton.addEventListener('click', function() {
-        audio.loop = true; // Set the audio to loop
-        audio.play(); // Start playing the audio
-        speakerIcon.classList.add('shake'); // Add the shake class to the image
+    playButton.addEventListener('click', function () {
+        audio.loop = true;
+        audio.play();
+        speakerIcon.classList.add('shake');
         para.classList.add('red-text');
 
     });
 
-    stopButton.addEventListener('click', function() {
-        audio.pause(); // Pause the audio
-        audio.currentTime = 0; // Reset the audio to the beginning
-        speakerIcon.classList.remove('shake'); // Remove the shake class from the image
+    stopButton.addEventListener('click', function () {
+        audio.pause();
+        audio.currentTime = 0;
+        speakerIcon.classList.remove('shake');
         para.classList.remove('red-text');
 
     });
